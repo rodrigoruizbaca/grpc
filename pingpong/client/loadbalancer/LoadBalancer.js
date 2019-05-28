36589f43280b46e8774699077eeb918ab24b8655
Rodrigo Ruiz <rodrigo.ruiz.baca@gmail.com>
2019-05-28 08:16:38 -0500

    Make loadbalancer better

--- pingpong/client/loadbalancer/LoadBalancer.js
+++ pingpong/client/loadbalancer/LoadBalancer.js
@@ -27,18 +27,26 @@ module.exports = class LoadBalancer {
                 const serviceName = s.Attributes.ECS_SERVICE_NAME;  
                 const status = s.Attributes.AWS_INIT_HEALTH_STATUS;
                 const client = new services.PingPongClient(url, grpc.credentials.createInsecure());
-                const obj = {
+                const endpoint = {
                     url, serviceName, status, client
                 }; 
-                this.services.push(obj);            
+                if (!this.services[serviceName]) {
+                    const service = {
+                        next: 0,
+                        endpoints: []
+                    };
+                    this.services[serviceName] = service;
+                } 
+                this.services[serviceName].endpoints.push(endpoint);
             });
         });
     }
 
     getEndpoint(serviceName) {
-        const possible = _.filter(this.services, ['serviceName', serviceName]);
-        const idx = (this.next++) % possible.length;
-        return possible[idx];
+        const service = this.services[serviceName];
+        const endpoints = service.endpoints;
+        const idx = (service.next++) % endpoints.length;
+        return endpoints[idx];
     } 
 
     list() {
